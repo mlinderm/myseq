49b7f18b195db374d5de784870d6f11bb7a742f9
Michael Linderman <mlinderman@middlebury.edu>
2018-08-28 12:57:26 -0400

    Streamline ancestry bayesian analysis

--- src/MySeq.js
+++ src/MySeq.js
@@ -13,8 +13,7 @@ import VariantQuery from './components/query/VariantQuery';
 import Traits from './components/traits/Traits';
 import PGx from './components/pgx/PGx';
 import Risks from './components/risks/Risks';
-import AncestryPCA from './components/ancestry/AncestryPCA';
-import ChromosomePainting from './components/chromosomepainting/painting';
+import Ancestry from './components/ancestry/Ancestry';
 
 class MySeq extends Component {
   constructor(props) {
@@ -80,8 +79,7 @@ class MySeq extends Component {
                   <Route path="/traits" component={Traits} />
                   <Route path="/pgx" component={PGx} />
                   <Route path="/risks" component={Risks} />
-                  <SourceRoute path="/ancestry" exact component={AncestryPCA} />
-                  <SourceRoute path="/chromosomepainting" exact component={ChromosomePainting} />
+                  <Route path="/ancestry" component={Ancestry} />
                 </Switch>
               </Container>
             </main>
---
+++ src/components/ancestry/Ancestry.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import AnalysisSelector from '../AnalysisSelector';
+
+import AncestryPCA from './AncestryPCA';
+import ChromosomePainting from './ChromosomePainting';
+
+const risks = [
+  { title: 'PCA Analysis', route: '/ancestry/pca', component: AncestryPCA },
+  { title: 'Bayesian Analysis', route: '/ancestry/painting', component: ChromosomePainting },
+];
+
+export default function Ancestry(props) {
+  return (
+    <AnalysisSelector {...props} analyses={risks} category="Ancestry" categoryPath="/ancestry" dropdownText="Choose analysis" />
+  );
+}
