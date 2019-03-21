4125e9d61218001b9e2440bdd7bf8ebf94e5a3f9
Michael Linderman <mlinderman@middlebury.edu>
2019-03-21 09:37:52 -0400

    Update to CRA 2+ and update other dependencies

--- config-overrides.js
+++ config-overrides.js
@@ -2,27 +2,18 @@
 const path = require('path');
 
 module.exports = function override(config) {
-  if (process.env.npm_lifecycle_script.includes('embed')) {
-    config.output.library = 'myseq';
-    config.output.libraryTarget = 'umd';
-    config.output.umdNamedDefine = true;
-    config.output.libraryExport = 'default';
-
-    /**
-     * Change the webpack entry and output path
-     */
-    config.entry = path.resolve('src/index-embed.js');
-    config.output.filename = 'myseq-embed.js';
-    config.output.path = path.resolve('build');
-  }
-
   // Adapted from:
   // https://medium.com/@danilog1905/how-to-use-web-workers-with-react-create-app-and-not-ejecting-in-the-attempt-3718d2a1166b
+  // https://github.com/arackaf/customize-cra
   config.module.rules.push({
     test: /\.worker\.js$/,
     use: { loader: 'worker-loader' },
   });
 
+  // Note: Here is a side effect.
+	// @see https://github.com/webpack/webpack/issues/6642#issuecomment-370222543
+	const { output } = config;
+	output.globalObject = 'this';
 
   return config;
 };
