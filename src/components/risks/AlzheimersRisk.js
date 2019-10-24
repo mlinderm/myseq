2edaf953d6b3a3c65ff652be2ab60c13e0013ad9
Michael Linderman <mlinderman@middlebury.edu>
2019-10-24 14:37:32 -0400

    Revise documentation

--- src/components/risks/AlzheimersRisk.js
+++ src/components/risks/AlzheimersRisk.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import MultiVariantTrait from '../analyses/MultiVariantTrait';
-import { SNPedia } from '../util/links';
+import { SNPedia, ExternalLink } from '../util/links';
 
 const MonoSpan = styled.span.attrs({
   className: 'text-monospace'
@@ -48,11 +48,19 @@ export default function AlzheimersRisk() {
         Alzheimer&apos;s Disease. Alzheimer&apos;s disease is a progressive,
         degenerative disorder that attacks the brain&apos;s nerve cells, or
         neurons, resulting in loss of memory, thinking and language skills, and
-        behavioral changes. One or more copies of the ε4 allele increases the
-        risk of developing Alzheimer&apos;s Disease. The ε2 allele is rare and
-        may have protective effects. The ε2, ε3, and ε4 alleles are determined
-        by the combination of these two variants on the same chromosome. Since
-        the <MonoSpan>rs429358(C)</MonoSpan> + <MonoSpan>rs7412(T)</MonoSpan>{' '}
+        behavioral changes. One or more copies of the ε4 allele is associated
+        with increased risk of developing Alzheimer&apos;s Disease. The ε2
+        allele is rare and may have protective effects. However as described in{' '}
+        <ExternalLink href="https://www.ncbi.nlm.nih.gov/books/NBK1161/">
+          GeneReviews
+        </ExternalLink>{' '}
+        the ε4 allele is "not sufficient to cause disease" and "while{' '}
+        <i>APOE</i> genotyping may have an adjunct role in the diagnosis of
+        [Alzheimer&apos;s Disease] in symptomatic individuals, it appears to
+        have little role at this time in predictive testing of asymptomatic
+        individuals". The ε2, ε3, and ε4 alleles are determined by the
+        combination of these two variants on the same chromosome. Since the{' '}
+        <MonoSpan>rs429358(C)</MonoSpan> + <MonoSpan>rs7412(T)</MonoSpan>{' '}
         haplotype is extremely rare, individuals with <MonoSpan>C/T</MonoSpan>{' '}
         genotypes at both SNPs are assumed to be ε2/ε4. Adapted from{' '}
         <SNPedia title="APOE-ε4" oldid={1536640} />.
